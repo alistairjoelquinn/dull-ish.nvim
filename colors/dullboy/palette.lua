f6dfdff3c0710b0f559745eddca5787449077aee
alistairjoelquinn <alistairjoelquinn@gmail.com>
2025-01-07 13:22:09 +0000

    refactor to move palette into separate file

---
+++ colors/dullboy/palette.lua
@@ -0,0 +1,19 @@
+return {
+  -- base colors
+  bg = "#202328", -- slightly lighter dark background
+  bg_lighter = "#252830", -- for subtle highlights
+  fg = "#999999", -- muted grey for main text
+  fg_darker = "#777777", -- darker grey for less emphasis
+  fg_lighter = "#aaaaaa", -- lighter grey for emphasis
+
+  -- subtle accent colors (all very desaturated)
+  subtle_yellow = "#FFE751", -- yellow
+  subtle_orange = "#8b7355", -- muted orange
+  subtle_green = "#657163", -- muted green
+  subtle_blue = "#616b7b", -- muted blue
+
+  -- ui colors
+  ui_grey = "#464646", -- for ui elements
+  ui_grey_lighter = "#565656",
+  comment_grey = "#5a5a5a", -- for comments
+}
