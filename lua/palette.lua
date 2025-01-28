5c06e0cf3f6e53e8e9e37b5b65addd10edce83ce
alistairjoelquinn <alistairjoelquinn@gmail.com>
2025-01-28 10:58:05 +0100

    slightly dulls the yellow

--- lua/palette.lua
+++ lua/palette.lua
@@ -11,7 +11,7 @@ return {
   -- accent colors
   -- subtle_yellow = "#A69636", -- original yellow
   -- subtle_yellow = "#d0bd45", -- lighter yellow
-  subtle_yellow = "#efd749", -- lighter yellow
+  subtle_yellow = "#d2c266", -- yellower yellow
   subtle_orange = "#b77e89",
   subtle_green = "#7a8574",
   subtle_blue = "#7B8FA6",
